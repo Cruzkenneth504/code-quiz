b137366d536878eb54e569c959a053c4e95570df
kenneth o cruz <cruzkenneth504@gmail.com>
2023-04-02 14:28:18 -0700

    Jss var input

---
+++ assets/index.js
@@ -0,0 +1,76 @@
+var questions = [
+{
+quest: "Choose the correct HTML element to define important text",
+choices: ["<b> ", "<i> ", "<strong> ", "<important> "],
+Correct: "Answer <strong>",
+
+},
+
+{
+quest:"Choose the correct HTML element to define emphasized text",
+choices: ["<em> ", "<i> ", "<italic> ", "<a> "],
+Correct: "Answer <em>",
+
+},
+
+{
+quest:"Which character is used to indicate an end tag?",
+choices:["< ", "/ ", "^ "],
+correct: "Answer < "
+},
+
+{
+  quest:"How can you make a numbered list?",
+  choices:["<li> ", "<td> ", "<ol> ", "<ul> "],
+  correct:"Answer <ol>",
+},
+
+{
+
+  quest:"Which is the correct CSS syntax?",
+  choices:["{body:color,black} ", "body:color(black ", "body {color:black } ", "body=color:black "],
+  correct:"Answer body {color:black }",
+},
+
+{
+quest:"Which property is used to change the background color?",
+choices:[" bg  ", "background-color ", "color  ", "block color"],
+correct:"Answer background-color",
+ 
+},
+
+{
+
+quest:"Which CSS property is used to change the text color of an element?",
+choices:["letter color ", "font-color ", "text-color ", "color "],
+correct:"Answer color",
+
+},
+
+{
+
+quest: "Inside which HTML element do we put the JavaScript?",
+choices: ["<html> ", "<js ", "<JavaScript>, ", "<script> "],
+Correct: "Answer <script>",
+
+},
+
+{
+quest:"How do you write 'Hello World' in an alert box?",
+choices:[" alert('Hello World') ", "alertWindow ('Hello World') ", "msgAlert('Hello World') ", " aletPop('Hello World')"],
+correct:"Answer alert('Hello World')",
+
+},
+
+{
+
+quest:"How do you create a function in JavaScript?",
+choices:[" funtion{} ", "funtion = () ", "function myFuntion() ", "let fuction = myFuntion[] "],
+correct:"Answer funtiom myFunction()",
+
+},
+
+]
+
+
+
