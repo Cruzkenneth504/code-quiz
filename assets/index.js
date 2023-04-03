87f4f5642617e075ee21e05bebbb53a506207a6f
kenneth o cruz <cruzkenneth504@gmail.com>
2023-04-02 20:37:59 -0700

    JSS/HTML/CSS Index finished

--- assets/index.js
+++ assets/index.js
@@ -1,108 +1,193 @@
 //Question Chart//
 var questions = [
-{
-quest: "Choose the correct HTML element to define important text",
-choices: ["<b> ", "<i> ", "<strong> ", "<important> "],
-Correct: "Answer: <strong>",
+    {
+        title: "Which character is used to indicate an end tag",
+        choices: ["<", "*", "$", "</>"],
+        answer: "</>"
+    },
+    {
+        title: "How can you make a numbered list?",
+        choices: ["<li>", "<td>", "<ol>", "<ul>"],
+        answer: "<ol>"
+    },
+    {
+        title: "Which is the correct CSS syntax?",
+        choices: ["{body:color,black}", "body:color(black", "body {color:black}", "body=color:black"],
+        answer: "body {color:black}"
+    },
+    {
+        title: "Which property is used to change the background color",
+        choices: ["bg", "background-color", "color", "block color"],
+        answer: "background-color",
+    },
+    {
+        title: "Which CSS property is used to change the text color of an element?",
+        choices: ["letter color ", "font-color ", "text-color ", "color "],
+        answer: "color",
+    },
 
-},
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<html>", "<js>", "<JavaScript>", "<script>"],
+        answer: "<script>",
+        
+    },
 
-{
-quest:"Choose the correct HTML element to define emphasized text",
-choices:["<em> ", "<i> ", "<italic> ", "<a> "],
-Correct: "Answer: <em>",
-
-},
-
-{
-quest:"Which character is used to indicate an end tag?",
-choices:["< ", "/ ", "^ "],
-correct: "Answer: < "
-},
-
-{
-  quest:"How can you make a numbered list?",
-  choices:["<li> ", "<td> ", "<ol> ", "<ul> "],
-  correct:"Answer: <ol>",
-},
-
-{
-
-  quest:"Which is the correct CSS syntax?",
-  choices:["{body:color,black} ", "body:color(black ", "body {color:black } ", "body=color:black "],
-  correct:"Answer: body {color:black }",
-},
-
-{
-quest:"Which property is used to change the background color?",
-choices:[" bg  ", "background-color ", "color  ", "block color"],
-correct:"Answer: background-color",
- 
-},
-
-{
-
-quest:"Which CSS property is used to change the text color of an element?",
-choices:["letter color ", "font-color ", "text-color ", "color "],
-correct:"Answer: color",
-
-},
-
-{
-
-quest: "Inside which HTML element do we put the JavaScript?",
-choices: ["<html> ", "<js ", "<JavaScript>, ", "<script> "],
-Correct: "Answer: <script>",
-
-},
-
-{
-quest:"How do you write 'Hello World' in an alert box?",
-choices:[" alert('Hello World') ", "alertWindow ('Hello World') ", "msgAlert('Hello World') ", " aletPop('Hello World')"],
-correct:"Answer: alert('Hello World')",
-
-},
-
-{
-
-quest:"How do you create a function in JavaScript?",
-choices:[" funtion{} ", "funtion = () ", "function myFuntion() ", "let fuction = myFuntion[] "],
-correct:"Answer: funtiom myFunction()",
-
-},
+    {
+        title:"How do you write 'Hello World' in an alert box?",
+        choices:[" alert('Hello World')", "alertWindow ('Hello World')", "msgAlert('Hello World')", "aletPop('Hello World')"],
+        answer:"alert('Hello World')",
+    }
 
 ];
 
 var score = 0;
 var questionIndex = 0;
 
-var timer = document.querySelector("#start-btn");
-var currentTime = document.querySelector("#current-time");
-var questContent = document.querySelector("#quest-container");
-var mainContent = document.querySelector("#main-container");
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
 
-var secleft = 76;
+var secondsLeft = 76;
 var holdInterval = 0;
-var penalty= 10;
-var createUl= document.createElement("ul");
+var penalty = 10;
+var ulCreate = document.createElement("ul");
 
-timer.addEventListener("click",function(){
-  if (holdInterval === 0){
-      holdInterval = setInterval(function () {
-      secleft--;
-      currentTime.textContent = "Time " + secleft;
 
-      if(secleft <= 0) {
-        clearInterval(holdInterval);
-        allDone();
-        currentTime.textContent = "Game Over";
-      }
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
 
-    }, 1000)
-  } 
-  render(questionIndex)
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
 });
 
-function render(questionIndex){
-  
+
+function render(questionIndex) {
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+    
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+        } else {
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        allDone();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+
+function allDone() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+    questionsDiv.appendChild(createH1);
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questionsDiv.appendChild(createP2);
+    }
+
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            window.location.replace("./HighScores.html");
+        }
+    });
+
 }
